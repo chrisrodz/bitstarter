92fd9d1dab44f85f0170b38e7a8603f2959c25c4
Christian <christian.etpr10@gmail.com>
2014-11-22 14:58:37 -0400

    Hello from index.html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
+var fs = require('fs')
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  var buf = new Buffer(fs.readFileSync('index.html'));
+  response.send(buf.toString());
 })
 
 app.listen(app.get('port'), function() {
